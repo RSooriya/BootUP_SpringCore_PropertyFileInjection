8b79e74ebc8db949b0d33da6ea4d4c60f328d574
RSooriya <soormo06@gmail.com>
2020-06-30 17:07:20 +0530

    Comments added

--- src/com/sooriya/MyApp.java
+++ src/com/sooriya/MyApp.java
@@ -5,9 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyApp {
 	public static void main(String args[])
 	{
+		//creating spring container and loading configuration file into container
 		ClassPathXmlApplicationContext context = new 
 				ClassPathXmlApplicationContext("applicationContext.xml");
+		//retrieving beans from container
 		CricketCoach theCoach = context.getBean("myCoach",CricketCoach.class);
+		//calling methods on bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getEmail());
 		System.out.println(theCoach.getTeam());
